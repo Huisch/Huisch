3ca693a306183354c43bcf08ba47a7c8daece10b
Tim Nederveen <jptned@hotmail.nl>
2019-11-03 22:11:21 +0100

    Startcommando

---
+++ src/TelegramCommands/StartCommand.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\TelegramCommands;
+
+use Longman\TelegramBot\Commands\UserCommand;
+use Longman\TelegramBot\Entities\ServerResponse;
+use Longman\TelegramBot\Exception\TelegramException;
+use Longman\TelegramBot\Request;
+
+/**
+ * Start command
+ */
+class StartCommand extends UserCommand
+{
+    /**
+     * @var string
+     */
+    protected $name = 'start';
+
+    /**
+     * @var string
+     */
+    protected $description = 'Start command';
+
+    /**
+     * @var string
+     */
+    protected $usage = '/start';
+
+    /**
+     * @var string
+     */
+    protected $version = '1.1.0';
+
+    /**
+     * Command execute method
+     *
+     * @return ServerResponse
+     * @throws TelegramException
+     */
+    public function execute()
+    {
+        $message = $this->getMessage();
+        $chat_id = $message->getChat()->getId();
+
+        return Request::sendMessage([
+			'chat_id' => $chat_id,
+			'text' => "Welkom! Voeg me toe aan een groepsapp om me te gebruiken."
+		]);
+    }
+}
